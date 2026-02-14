974eaf12b0dc4dcb33552d71e42af82a65be59fe
Stealinp <stealinp@gmail.com>
2026-02-15 02:09:40 +0800

    Add control for third LED on pin 11

--- 3led.ino
+++ 3led.ino
@@ -1,8 +1,10 @@
 int pin1=9;
-int pin2=11;
+int pin2=10;
+int pin3=11;
 void setup() {
   pinMode(pin1,OUTPUT);
   pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
 }
 
 void loop() {
@@ -14,6 +16,7 @@ void loop() {
     analogWrite(pin1,i);
     delay(10);
   }
+
   for(int i=0;i<=255;i=i+5){
     analogWrite(pin2,i);
     delay(10);
@@ -22,4 +25,12 @@ void loop() {
     analogWrite(pin2,i);
     delay(10);
   }
-}
+
+  for(int i=0;i<=255;i=i+5){
+    analogWrite(pin3,i);
+    delay(10);
+  }
+  for(int i=255;i>=0;i=i-5){
+    analogWrite(pin3,i);
+    delay(10);
+  }
