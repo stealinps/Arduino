7fa8723c705cf7771a29220cd2251f075ab385cc
Stealinp <stealinp@gmail.com>
2026-02-16 02:15:11 +0800

    Enhance LED fading with RGB color transitions
    Refactor LED fading logic to use RGB values for smoother transitions.

--- 3ledFade.ino
+++ 3ledFade.ino
@@ -1,24 +1,44 @@
-int pin1=9;
-int pin2=10;
-int pin3=11;
-int value;
+int pin1 = 9;
+int pin2 = 10;
+int pin3 = 11;
+
+int redVal = 255;
+int greenVal = 0;
+int blueVal = 0;
+
+int i = 0;
+
 void setup() {
-  pinMode(pin1,OUTPUT);
-  pinMode(pin2,OUTPUT);
-  pinMode(pin3,OUTPUT);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
 }
 
 void loop() {
-  for(value=0;value<=255;value++){
-    analogWrite(pin1,value);
-    analogWrite(pin2,value);
-    analogWrite(pin3,value);
-    delay(10);
+
+  if (i < 255) {
+    redVal--;
+    greenVal++;
+    blueVal = 0;
   }
-  for(value=255;value>=0;value--){
-    analogWrite(pin1,value);
-    analogWrite(pin2,value);
-    analogWrite(pin3,value);
-    delay(10);
+  else if (i < 510) {
+    redVal = 0;
+    greenVal--;
+    blueVal++;
   }
+  else if (i < 765) {
+    redVal++;
+    greenVal = 0;
+    blueVal--;
+  }
+  else {
+    i = 0; 
+  }
+
+  analogWrite(pin1, redVal);
+  analogWrite(pin2, greenVal);
+  analogWrite(pin3, blueVal);
+
+  i++;
+  delay(10);
 }
