aca863dbc4449734c174fc931c12d5c25c83b205
Stealinp <stealinp@gmail.com>
2026-02-16 02:30:00 +0800

    Modify LED fading colors and adjust values

--- 3ledFade.ino
+++ 3ledFade.ino
@@ -2,9 +2,9 @@ int pin1 = 9;
 int pin2 = 10;
 int pin3 = 11;
 
-int redVal = 255;
+int redVal = 0;
 int greenVal = 0;
-int blueVal = 0;
+int yelVal = 0;
 
 int i = 0;
 
@@ -12,6 +12,15 @@ void setup() {
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
+  delay(500);
+  for(i=0;i<255;i++){
+    redVal++;
+    analogWrite(pin1, redVal);
+    analogWrite(pin2, greenVal);
+    analogWrite(pin3, yelVal);
+    delay(20);
+  }
+  i=0;
 }
 
 void loop() {
@@ -19,26 +28,29 @@ void loop() {
   if (i < 255) {
     redVal--;
     greenVal++;
-    blueVal = 0;
+    yelVal = 0;
   }
   else if (i < 510) {
     redVal = 0;
     greenVal--;
-    blueVal++;
+    yelVal++;
   }
   else if (i < 765) {
     redVal++;
     greenVal = 0;
-    blueVal--;
+    yelVal--;
   }
   else {
     i = 0; 
+    redVal=255;
+    greenVal = 0;
+    yelVal=0;
   }
 
   analogWrite(pin1, redVal);
   analogWrite(pin2, greenVal);
-  analogWrite(pin3, blueVal);
+  analogWrite(pin3, yelVal);
 
   i++;
-  delay(10);
+  delay(20);
 }
