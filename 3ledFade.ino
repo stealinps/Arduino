328f60d002397d5a100bbfee9c6ace43096729e1
Stealinp <stealinp@gmail.com>
2026-02-16 03:20:14 +0800

    Refactor LED fading logic in 3ledFade.ino using switch case
    Refactor LED fading logic and improve variable initialization.

--- 3ledFade.ino
+++ 3ledFade.ino
@@ -6,51 +6,46 @@ int redVal = 0;
 int greenVal = 0;
 int yelVal = 0;
 
-int i = 0;
+int i;
 
 void setup() {
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
-  delay(500);
-  for(i=0;i<255;i++){
-    redVal++;
+  
+  for(i = 0; i < 255; i++){
     analogWrite(pin1, redVal);
-    analogWrite(pin2, greenVal);
-    analogWrite(pin3, yelVal);
-    delay(20);
+    redVal++;
+    delay(10); 
   }
   i=0;
 }
 
 void loop() {
+  int phase = i / 255;
+  int step = i % 255;
 
-  if (i < 255) {
-    redVal--;
-    greenVal++;
-    yelVal = 0;
-  }
-  else if (i < 510) {
-    redVal = 0;
-    greenVal--;
-    yelVal++;
-  }
-  else if (i < 765) {
-    redVal++;
-    greenVal = 0;
-    yelVal--;
-  }
-  else {
-    i = 0; 
-    redVal=255;
-    greenVal = 0;
-    yelVal=0;
+  switch (phase) {
+    case 0: 
+      redVal   = 255 - step;
+      greenVal = step;
+      yelVal  = 0;
+      break;
+    case 1: 
+      redVal   = 0;
+      greenVal = 255 - step;
+      yelVal  = step;
+      break;
+    case 2: 
+      redVal   = step;
+      greenVal = 0;
+      yelVal  = 255 - step;
+      break;
   }
 
   analogWrite(pin1, redVal);
   analogWrite(pin2, greenVal);
   analogWrite(pin3, yelVal);
-
-  i++;
-  delay(20);
+  i = (i + 1) % 765; 
+  delay(10);
 }
